51eff4c46ea4306ef2799995bfb762deb4c37cf0
WMcD <mcder017@users.noreply.github.com>
2024-12-05 01:36:21 -0600

    Initial implementation of using socket access to extract and display time information. Shows an arbitrary initial string,  shows running seconds, and shows final time.  Core code all within main() method, not yet  separated into nice functions and classes.

--- display/led-timer-display.cc
+++ display/led-timer-display.cc
@@ -18,21 +18,32 @@
 #include "led-matrix.h"
 #include "graphics.h"
 
-#include <algorithm>
-#include <fstream>
-#include <streambuf>
-#include <string>
+#include "bdf-10x20-local.h"
 
-#include <getopt.h>
-#include <math.h>
+//#include <algorithm>
+//#include <fstream>
+//#include <streambuf>
+//#include <string>
+
+#include <getopt.h>  // for command line options
+#include <math.h>    // for fabs
 #include <signal.h>
-#include <stdint.h>
+//#include <stdint.h>
+#include <string.h>
+//#include <sys/stat.h>
+
+#include <time.h>    // for monitoring clock for steady scrolling
+#include <unistd.h>  // for option parsing; sleep
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <time.h>
-//#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>  // inet_ntoa
+
+#include <sstream>  // stringstream
+#include <ios>
 
 using namespace rgb_matrix;
 
@@ -41,13 +52,18 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
+static const int TCP_PORT_DEFAULT = 21967;
+static const char *LED_ERROR_MESSAGE_SOCKET = "P-ERR1";
+static const char *LED_ERROR_MESSAGE_BIND = "P-ERR2";
+static const char *LED_ERROR_MESSAGE_ACCEPT = "P-ERR3";
+static const char *LED_ERROR_MESSAGE_SOCKET_OPTIONS = "P-ERR4";
+
 static int usage(const char *progname) {
   fprintf(stderr, "usage: %s [options] [<text>| -i <filename>]\n", progname);
   fprintf(stderr, "Takes text and scrolls it with speed -s\n");
   fprintf(stderr, "Options:\n");
   fprintf(stderr,
           "\t-f <font-file>    : Path to *.bdf-font to be used.\n"
-          "\t-i <textfile>     : Input from file.\n"
           "\t-s <speed>        : Approximate letters per second. \n"
           "\t                    Positive: scroll right to left; Negative: scroll left to right\n"
           "\t                    (Zero for no scrolling)\n"
@@ -60,11 +76,23 @@ static int usage(const char *progname) {
           "\t-t <track-spacing>: Spacing pixels between letters (Default: 0)\n"
           "\n"
           "\t-C <r,g,b>        : Text Color. Default 255,255,255 (white)\n"
-          "\t-B <r,g,b>        : Background-Color. Default 0,0,0\n"
+          "\t-B <r,g,b>        : Background-Color. Default 0,0,0 (black)\n"
           "\t-O <r,g,b>        : Outline-Color, e.g. to increase contrast.\n"
           );
   fprintf(stderr, "\nGeneral LED matrix options:\n");
   rgb_matrix::PrintMatrixFlags(stderr);
+  fprintf(stderr, "\nTCP configuration:\n");
+  fprintf(stderr,
+          "\t-p <portnumber>   : TCP port number (default 21967)\n"
+          );
+  fprintf(stderr, "\nOne-step configurations:\n");
+  fprintf(stderr,
+          "\t-Q                : Quick configuration with\n"
+          "\t                    row 16, cols 32, chain 3, parallel 1,\n"
+          "\t                    GPIO slowdown 2, GPIO map adafruit-hat-pwm,\n"
+          "\t                    font 10x20, text red (255,0,0), y-offset -2, track spacing -1\n"
+          "\t                    speed 0\n"
+          );
   return 1;
 }
 
@@ -93,30 +121,31 @@ static void do_pause() {
   sleep(3);
 }
 
-// Read line and return if it changed.
-typedef uint64_t stat_fingerprint_t;
-static bool ReadLineOnChange(const char *filename, std::string *out,
-                             stat_fingerprint_t *last_file_status) {
-  struct stat sb;
-  if (stat(filename, &sb) < 0) {
-    perror("Couldn't determine file change");
-    return false;
+/**
+ * Prints a string to the screen, with non-printable characters displayed in hexadecimal form.
+ *
+ * @param str: The string to be printed.
+ */
+static void printStringWithHexadecimal(char* str) {
+  // Iterate through each character in the string.
+  for (int i = 0; str[i] != '\0'; i++) {
+    // Check if the character is printable.
+    if (isprint(str[i])) {
+      printf("%c", str[i]);  // Print the character as is.
+    } else {
+      printf("\\x%02x", (unsigned char) str[i]);  // Print the character in hexadecimal form.
+    }
   }
-  const stat_fingerprint_t fp = ((uint64_t)sb.st_mtime << 32) + sb.st_size;
-  if (fp == *last_file_status) {
-    return false;  // no change according to stat()
-  }
+}
 
-  *last_file_status = fp;
-  std::ifstream fs(filename);
-  std::string str((std::istreambuf_iterator<char>(fs)),
-                  std::istreambuf_iterator<char>());
-  std::replace(str.begin(), str.end(), '\n', ' ');
-  if (*out == str) {
-    return false;  // no content change
+static void replaceNonPrintableCharacters(char *str, char repl_char) {
+  // Iterate through each character in the string.
+  for (int i = 0; str[i] != '\0'; i++) {
+    // Check if the character is printable.
+    if (!isprint(str[i])) {
+      str[i] = repl_char;
+    }
   }
-  *out = str;
-  return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -138,19 +167,28 @@ int main(int argc, char *argv[]) {
   bool with_outline = false;
 
   const char *bdf_font_file = NULL;
-  const char *input_file = NULL;
-  std::string line;
+  const char *EMPTY_STRING = "";
+  std::string line(EMPTY_STRING);      // default empty string displayed
   bool xorigin_configured = false;
   int x_orig = 0;
   int y_orig = 0;
+  int loops_orig = 0;
+
   int letter_spacing = 0;
   float speed = 7.0f;
   int loops = -1;
   int blink_on = 0;
   int blink_off = 0;
 
+  int sockfd = -1, newsockfd = -1;  // initialize sockets as not valid
+  socklen_t clilen;
+  const uint32_t TCP_BUFFER_SIZE = 96;
+  char socket_buffer[TCP_BUFFER_SIZE];
+  struct sockaddr_in serv_addr, cli_addr;
+  int port_number = TCP_PORT_DEFAULT;
+
   int opt;
-  while ((opt = getopt(argc, argv, "x:y:f:C:B:O:t:s:l:b:i:")) != -1) {
+  while ((opt = getopt(argc, argv, "x:y:f:C:B:O:t:s:l:b:p:Q")) != -1) {  // ':' suffix indicates required argument
     switch (opt) {
     case 's': speed = atof(optarg); break;
     case 'b':
@@ -159,11 +197,10 @@ int main(int argc, char *argv[]) {
       }
       fprintf(stderr, "hz: on=%d off=%d\n", blink_on, blink_off);
       break;
-    case 'l': loops = atoi(optarg); break;
+    case 'l': loops_orig = atoi(optarg); break;
     case 'x': x_orig = atoi(optarg); xorigin_configured = true; break;
     case 'y': y_orig = atoi(optarg); break;
     case 'f': bdf_font_file = strdup(optarg); break;
-    case 'i': input_file = strdup(optarg); break;
     case 't': letter_spacing = atoi(optarg); break;
     case 'C':
       if (!parseColor(&color, optarg)) {
@@ -184,40 +221,50 @@ int main(int argc, char *argv[]) {
       }
       with_outline = true;
       break;
+    case 'p': port_number = atoi(optarg); break;
+    case 'Q':
+      matrix_options.rows = 16;
+      matrix_options.cols = 32;
+      matrix_options.chain_length = 3;
+      matrix_options.parallel = 1;
+
+      runtime_opt.gpio_slowdown = 2;
+      matrix_options.hardware_mapping = "adafruit-hat-pwm";
+
+      bdf_font_file = NULL;  // use default 10x20 font, loaded below
+      color.setColor(255,0,0);  // red
+      letter_spacing = -1;
+      y_orig = -2;
+
+      speed = (float)0;
+      break;
     default:
       return usage(argv[0]);
     }
   }
 
-  stat_fingerprint_t last_change = 0;
-
-  if (input_file) {
-    if (!ReadLineOnChange(input_file, &line, &last_change)) {
-      fprintf(stderr, "Couldn't read file '%s'\n", input_file);
-      return usage(argv[0]);
-    }
-  }
-  else {
-    for (int i = optind; i < argc; ++i) {
-      line.append(argv[i]).append(" ");
-    }
-
-    if (line.empty()) {
-      fprintf(stderr, "Add the text you want to print on the command-line or -i for input file.\n");
-      return usage(argv[0]);
-    }
+  // check for any initial string to display, limited by whitespace being single spaces
+  for (int i = optind; i < argc; ++i) {
+    line.append(argv[i]).append(" ");
   }
 
-  if (bdf_font_file == NULL) {
-    fprintf(stderr, "Need to specify BDF font-file with -f\n");
-    return usage(argv[0]);
-  }
+//  if (line.empty()) {
+//    fprintf(stderr, "Add the text you want to print on the command-line or -i for input file.\n");
+//    return usage(argv[0]);
+//  }
 
   /*
-   * Load font. This needs to be a filename with a bdf bitmap font.
+   * Load font. If using a file rather than the default, it needs to be a filename with a bdf bitmap font.
    */
   rgb_matrix::Font font;
-  if (!font.LoadFont(bdf_font_file)) {
+  if (bdf_font_file == NULL) {
+    // read built-in default font
+    if (!font.ReadFont(BDF_10X20_STRING)) {
+      fprintf(stderr, "Couldn't read default built-in 10x20 font\n");
+      return 1;
+    }
+  }
+  else if (!font.LoadFont(bdf_font_file)) {
     fprintf(stderr, "Couldn't load font '%s'\n", bdf_font_file);
     return 1;
   }
@@ -242,11 +289,6 @@ int main(int argc, char *argv[]) {
   if (all_extreme_colors)
     canvas->SetPWMBits(1);
 
-  signal(SIGTERM, InterruptHandler);
-  signal(SIGINT, InterruptHandler);
-
-  printf("CTRL-C for exit.\n");
-
   // Create a new canvas to be used with led_matrix_swap_on_vsync
   FrameCanvas *offscreen_canvas = canvas->CreateFrameCanvas();
 
@@ -266,62 +308,209 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  if (isatty(STDIN_FILENO)) {
+    // Only give a message if we are interactive. If connected via pipe, be quiet
+    printf("Setting up socket...\n");
+  }
+
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  bool socket_ok = !(sockfd < 0);
+  if (!socket_ok) {
+    fprintf(stderr, "socket() failed\n");
+    line = LED_ERROR_MESSAGE_SOCKET;
+  }
+  const int enable = 1;
+  if (socket_ok &&
+      (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0 ||
+       setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) < 0)) {
+    socket_ok = false;
+    fprintf(stderr, "setsockopt() failed\n");
+    line = LED_ERROR_MESSAGE_SOCKET_OPTIONS;
+  }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port_number);
+  if (socket_ok &&
+      bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) {
+    socket_ok = false;
+    fprintf(stderr, "bind(%d) failed\n", port_number);
+    line = LED_ERROR_MESSAGE_BIND;
+  }
+  if (socket_ok && isatty(STDIN_FILENO)) {
+    // Only give a message if we are interactive. If connected via pipe, be quiet
+    printf("Listening on port %d...\n", port_number);
+  }
+
   int x = x_orig;
   int y = y_orig;
   int length = 0;
 
+  // set up initial text before waiting for connection
+  offscreen_canvas->Fill(bg_color.r, bg_color.g, bg_color.b);
+  length = rgb_matrix::DrawText(offscreen_canvas, font,  // length = holds how many pixels our text takes up
+                                x, y + font.baseline(),
+                                color, NULL,
+                                line.c_str(), letter_spacing);
+
+  // Swap the offscreen_canvas with canvas on vsync, avoids flickering
+  offscreen_canvas = canvas->SwapOnVSync(offscreen_canvas);
+
+  if (socket_ok) {
+    const int MAX_PENDING_CONNECTION = 5;
+    listen(sockfd, MAX_PENDING_CONNECTION);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) {
+      socket_ok = false;
+      fprintf(stderr, "accept() failed\n");
+      line = LED_ERROR_MESSAGE_ACCEPT;
+    }
+    if (socket_ok && isatty(STDIN_FILENO)) {
+      // Only give a message if we are interactive. If connected via pipe, be quiet
+      printf("Connected to:%s\n",(cli_addr.sin_family == AF_INET ? inet_ntoa(cli_addr.sin_addr) : "(non-IPV4)"));
+    }
+  }
+  bzero(socket_buffer,TCP_BUFFER_SIZE);
+
   struct timespec next_frame = {0, 0};
 
-  uint64_t frame_counter = 0;
-  while (!interrupt_received && loops != 0) {
-    if (input_file && ReadLineOnChange(input_file, &line, &last_change)) {
-      x = x_orig;
-    }
-    ++frame_counter;
-    offscreen_canvas->Fill(bg_color.r, bg_color.g, bg_color.b);
-    const bool draw_on_frame = (blink_on <= 0)
-      || (frame_counter % (blink_on + blink_off) < (uint64_t)blink_on);
-
-    if (draw_on_frame) {
-      if (outline_font) {
-        // The outline font, we need to write with a negative (-2) text-spacing,
-        // as we want to have the same letter pitch as the regular text that
-        // we then write on top.
-        rgb_matrix::DrawText(offscreen_canvas, *outline_font,
-                             x - 1, y + font.baseline(),
-                             outline_color, NULL,
-                             line.c_str(), letter_spacing - 2);
-      }
-
-      // length = holds how many pixels our text takes up
-      length = rgb_matrix::DrawText(offscreen_canvas, font,
-                                    x, y + font.baseline(),
-                                    color, NULL,
-                                    line.c_str(), letter_spacing);
-    }
-
-    if (speed > 0) {
-      x += scroll_direction;
-      if ((scroll_direction < 0 && x + length < 0) ||
-          (scroll_direction > 0 && x > canvas->width())) {
-        x = x_orig + ((scroll_direction > 0) ? -length : 0);
-        if (loops > 0) --loops;
-      }
-
-      // Make sure render-time delays are not influencing scroll-time
-      if (next_frame.tv_sec == 0 && next_frame.tv_nsec == 0) {
-        // First time. Start timer, but don't wait.
-        clock_gettime(CLOCK_MONOTONIC, &next_frame);
-      } else {
-        add_micros(&next_frame, delay_speed_usec);
-        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_frame, NULL);
-      }
-    }
-
-    // Swap the offscreen_canvas with canvas on vsync, avoids flickering
-    offscreen_canvas = canvas->SwapOnVSync(offscreen_canvas);
-    if (speed <= 0) do_pause();  // Nothing to scroll. Delay a few seconds before looping
+  signal(SIGTERM, InterruptHandler);
+  signal(SIGINT, InterruptHandler);
+  if (isatty(STDIN_FILENO)) {
+    // Only give a message if we are interactive. If connected via pipe, be quiet
+    printf("CTRL-C for exit.\n");
   }
+
+  const char PROTOCOL_END_OF_LINE = '\x0d';
+  std::string tcp_unprocessed(EMPTY_STRING);  // buffer to accumulate unprocessed messages separated by newlines
+
+  uint64_t frame_counter = 0;
+  while (!interrupt_received) {
+    int num_read = (socket_ok ? recv(newsockfd, socket_buffer, TCP_BUFFER_SIZE-1, MSG_DONTWAIT) : 0);
+    if (num_read > 0) { // TBD: replace with logic if have new data to display (orig code monitored file for changes)
+
+      socket_buffer[num_read] = '\0';  // ensure end-of-string
+
+      if (isatty(STDIN_FILENO)) {
+        // Only give a message if we are interactive. If connected via pipe, be quiet
+        printf("Rcvd(len=%d):",num_read);
+        printStringWithHexadecimal(socket_buffer);
+        printf("\n");
+      }
+
+      // accumulate, so we can handle more than one protocol message in tcp buffer, or partial message in buffer
+      tcp_unprocessed.append(socket_buffer, num_read);
+    }
+
+    // if end-of-protocol message char is anywhere in the buffer, pull first line
+    std::string::size_type eol_pos = tcp_unprocessed.find_first_of(PROTOCOL_END_OF_LINE);
+    const bool found_line = eol_pos != std::string::npos;
+    if (found_line) {  // found the character
+      if (eol_pos >= TCP_BUFFER_SIZE-1) {
+        fprintf(stderr, "Line too long(%lu) in buffer:%s\n", (unsigned long)eol_pos, tcp_unprocessed.c_str());
+        eol_pos = TCP_BUFFER_SIZE - 2;
+      }
+      const int char_in_line = eol_pos+1;  // less than TCP_BUFFER_SIZE since eol_pos max TCP_BUFFER_SIZE-2;
+
+      char single_line_buffer[TCP_BUFFER_SIZE];
+      //bzero(single_line_buffer, TCP_BUFFER_SIZE);  // ensure end-of-string character will be present
+      tcp_unprocessed.copy(single_line_buffer, char_in_line, 0);  // copy char from beginning of string, incl eol
+      single_line_buffer[char_in_line] = '\0';  // less than TCP_BUFFER_SIZE since eol_pos max TCP_BUFFER_SIZE-2;
+      tcp_unprocessed.erase(0, char_in_line);                     // remove the characters from the string
+
+      if (isatty(STDIN_FILENO)) {
+        // Only give a message if we are interactive. If connected via pipe, be quiet
+        printf("Extracted line length %3lu, leaving %3lu unprocessed.\n",
+               (unsigned long)strlen(single_line_buffer),
+               (unsigned long)strlen(tcp_unprocessed.c_str()));
+      }
+
+      // TBD parse message and decide what to display
+      char msg_buffer[TCP_BUFFER_SIZE];
+      bzero(msg_buffer, TCP_BUFFER_SIZE);
+
+      if (strlen(single_line_buffer) >= 24) {  // possible ALGE protocol message
+        // TBD format nicely, such as with 00:01 for first second in running time message
+        strcpy(msg_buffer,single_line_buffer+12);  // start message at possible 1 minute character
+        msg_buffer[8] = '\0';  // end message at possible thousandth-of-second place
+      }
+      else {
+        strcpy(msg_buffer,single_line_buffer);
+      }
+      msg_buffer[11] = '\0';  // ensure message not longer
+      replaceNonPrintableCharacters(msg_buffer, '.');  // replace any non-printable characters
+      line = msg_buffer;  // copy into variable used to display to LED matrix
+      if (isatty(STDIN_FILENO)) {
+        // Only give a message if we are interactive. If connected via pipe, be quiet
+        printf("Displaying:%s\n",line.c_str());
+      }
+
+      // reset position and any looping information
+      x = x_orig;
+      y = y_orig;
+      loops = loops_orig;
+    }
+
+    if (!socket_ok || found_line || speed > 0) {
+      ++frame_counter;
+      offscreen_canvas->Fill(bg_color.r, bg_color.g, bg_color.b);
+      const bool draw_on_frame = (blink_on <= 0)
+        || (frame_counter % (blink_on + blink_off) < (uint64_t)blink_on);
+
+      if (draw_on_frame) {
+        if (outline_font) {
+          // The outline font, we need to write with a negative (-2) text-spacing,
+          // as we want to have the same letter pitch as the regular text that
+          // we then write on top.
+          rgb_matrix::DrawText(offscreen_canvas, *outline_font,
+                               x - 1, y + font.baseline(),
+                               outline_color, NULL,
+                               line.c_str(), letter_spacing - 2);
+        }
+
+        // length = holds how many pixels our text takes up
+        length = rgb_matrix::DrawText(offscreen_canvas, font,
+                                      x, y + font.baseline(),
+                                      color, NULL,
+                                      line.c_str(), letter_spacing);
+      }
+
+      if (speed > 0) {
+        x += scroll_direction;
+        if ((scroll_direction < 0 && x + length < 0) ||
+            (scroll_direction > 0 && x > canvas->width())) {
+          x = x_orig + ((scroll_direction > 0) ? -length : 0);
+          if (loops > 0) {
+            if (--loops == 0) {
+              line = EMPTY_STRING;  // clear the string being displayed, then wait for new text to display
+            }
+          }
+        }
+
+        // Make sure render-time delays are not influencing scroll-time
+        if (next_frame.tv_sec == 0 && next_frame.tv_nsec == 0) {
+          // First time. Start timer, but don't wait.
+          clock_gettime(CLOCK_MONOTONIC, &next_frame);
+        } else {
+          add_micros(&next_frame, delay_speed_usec);
+          clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_frame, NULL);
+        }
+      }
+
+      // Swap the offscreen_canvas with canvas on vsync, avoids flickering
+      offscreen_canvas = canvas->SwapOnVSync(offscreen_canvas);
+      if (!socket_ok && speed <= 0) do_pause();  // Nothing to scroll. Delay a few seconds before looping
+    }
+  }
+
+  // close sockets
+  // (main loop could be wrapped in try/catch)
+  if (newsockfd >= 0) close(newsockfd);
+  if (sockfd >= 0) close(sockfd);
 
   // Finished. Shut down the RGB matrix.
   canvas->Clear();
