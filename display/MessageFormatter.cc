bc96f52d28681016309dac74d0c15f32d6cbdce5
WMcD <mcder017@users.noreply.github.com>
2024-12-09 21:39:52 -0600

    Fix horizontal scroll setting

--- display/MessageFormatter.cc
+++ display/MessageFormatter.cc
@@ -12,7 +12,7 @@ static bool NO_VELOCITY_FOR_FIXED_TIMES = true;
 MessageFormatter::MessageFormatter(Displayer& aDisplayer, rgb_matrix::Font* aFontPtr, int aLetterSpacing,
                                    rgb_matrix::Color& fgColor, rgb_matrix::Color& bgColor,
                                    float velocity, bool scroll_horizontal, bool scroll_once)
-      : myDisplayer(aDisplayer), defaultVelocity(velocity), default_horizontal(scroll_once), default_once(scroll_once) {
+      : myDisplayer(aDisplayer), defaultVelocity(velocity), default_horizontal(scroll_horizontal), default_once(scroll_once) {
 
     defaultSpacedFont.fontPtr = aFontPtr;
     defaultSpacedFont.letterSpacing = aLetterSpacing;
