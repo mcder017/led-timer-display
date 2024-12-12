d77633e3f0e139da4cf4d250ac0a87b1ecbee38b
WMcD <mcder017@users.noreply.github.com>
2024-12-12 03:13:10 -0600

    Parse messages with board ID char, so running time and finish rank fields detected properly

--- display/MessageFormatter.cc
+++ display/MessageFormatter.cc
@@ -44,29 +44,46 @@ void MessageFormatter::handleAlgeMessage(const Receiver::RawMessage& message) {
 
   // parse fields from the message
 
-  constexpr size_t BIB_FIELD_LENGTH = 3;
-  std::string bibField = message.data.substr(0, BIB_FIELD_LENGTH);  // first three char may be bib number, or blank
-  bibField.erase(std::remove_if(bibField.begin(), bibField.end(), ::isspace), bibField.end());  // may now be empty string
-
-  const size_t RUNNING_FLAG_POS = 3;  // 4th char of protocol = string index 3
-  const char RUNNING_FLAG_CHAR = '.';
-  const bool isRunningTime = message.data.at(RUNNING_FLAG_POS) == RUNNING_FLAG_CHAR;
-
-  const size_t TIME_FIELD_POS = 8;    // 9th char of protocol = string index 8
-  const size_t TIME_FIELD_LENGTH = 12;  // hh:mm:ss.zht but leading or trailing part may be whitespace
   const size_t BOARD_IDENTIFIER_POS = 0;  // 1st char of protocol = string index 0
   const std::string BOARD_CHAR_STRING = "ABCDEFGHIJ";
   const bool isBoardIdentifier = BOARD_CHAR_STRING.find(message.data.at(BOARD_IDENTIFIER_POS)) != std::string::npos;
   const int field_pos_shift = isBoardIdentifier ? 1 : 0; // certain protocol messages have time field at col 10 (string location 9)
 
+  constexpr size_t BIB_FIELD_POS = 0;     // 1st char of protocol = string index 0; offset higher if Board ID
+  constexpr size_t BIB_FIELD_LENGTH = 3;
+  std::string bibField = message.data.substr(BIB_FIELD_POS+field_pos_shift, BIB_FIELD_LENGTH);  // first three char may be bib number, or blank
+  bibField.erase(std::remove_if(bibField.begin(), bibField.end(), ::isspace), bibField.end());  // may now be empty string
+
+  const size_t TIME_FIELD_POS = 8;    // 9th char of protocol = string index 8; offset higher if Board ID
+  const size_t TIME_FIELD_LENGTH = 12;  // hh:mm:ss.zht but leading or trailing part may be whitespace
   std::string timeField = message.data.substr(TIME_FIELD_POS+field_pos_shift, TIME_FIELD_LENGTH);
   timeField = trimWhitespace(timeField);
 
-  constexpr size_t RANK_FIELD_POS = 20;  // 21st char of protocol = string index 20
+  constexpr size_t RANK_FIELD_POS = 20;  // 21st char of protocol = string index 20; offset higher if Board ID
   constexpr size_t RANK_FIELD_LENGTH = 2;
   // may be rank number, or blank
-  std::string rankField = message.data.length() > RANK_FIELD_POS ? message.data.substr(RANK_FIELD_POS, RANK_FIELD_LENGTH) : "";
+  std::string rankField = message.data.length() > (RANK_FIELD_POS+field_pos_shift) ? message.data.substr(RANK_FIELD_POS+field_pos_shift, RANK_FIELD_LENGTH) : "";
   rankField = trimWhitespace(rankField);
+
+  // event type char (in TDC 4000 events) only potentially present if no board ID char
+  constexpr size_t EVENT_TYPE_POS = 3;  // 4th char of protocol = string index 3
+  char eventTypeChar = isBoardIdentifier ? ' ' : message.data.at(EVENT_TYPE_POS);
+
+  //constexpr size_t RUNNING_FLAG_POS_NO_BOARD_ID = 3;    // 4th char of protocol = string index 3
+  constexpr size_t FRAC_SECONDS_SEP_POS_WITH_BOARD_ID = 17; // 18th char of protocol = string index 17
+  constexpr char RUNNING_FLAG_CHAR = '.';
+  // handle both "usual" and "board ID" case
+  // in board ID case, the event flag field isn't used but no fractions of a second are recorded.  So look for separator, with non-blank seconds to ensure data present
+  // in non-board ID case, look for event flag field
+  const bool isStillRunningTime =
+    isBoardIdentifier ? (message.data.at(FRAC_SECONDS_SEP_POS_WITH_BOARD_ID-1) != ' '
+                          && message.data.at(FRAC_SECONDS_SEP_POS_WITH_BOARD_ID) != '.')
+                      : eventTypeChar == RUNNING_FLAG_CHAR;
+
+  const bool isIntermediateOne = eventTypeChar == 'A';
+  const bool isIntermediateTwo = eventTypeChar == 'B';
+  const bool isRunTime = eventTypeChar == 'C' || eventTypeChar == 'K';  // 'C' is TDC4000, 'K' is Comet Stopwatch (with next char identifying source Comet)
+  const bool isTotalTime = eventTypeChar == 'D' || (!isIntermediateOne && !isIntermediateTwo && !isStillRunningTime);
 
   // format the individual fields
 
@@ -110,15 +127,34 @@ void MessageFormatter::handleAlgeMessage(const Receiver::RawMessage& message) {
 
   // assemble the message to display
 
-  if (isRunningTime) {
+  if (isStillRunningTime) {
     const std::string text = "[ " + timeField + " ]";
     myDisplayer.startChangeOrder(buildDefaultChangeOrder(text.c_str()));
   }
-  else {
+  else if (isTotalTime) {
     // combine bib, time, and rank if provided
     const std::string text = //(bibField.empty() ? "" : bibField + "=") +
                              timeField
                              + (rankField.empty() ? "" : "(" + rankField + ")");
+    TextChangeOrder newOrder = buildDefaultChangeOrder(text.c_str());
+    if (NO_VELOCITY_FOR_FIXED_TIMES) newOrder.setVelocity(0);  // override velocity
+    myDisplayer.startChangeOrder(newOrder);
+  }
+  else if (isRunTime) {
+    // combine bib, time, and rank if provided
+    const std::string text = //(bibField.empty() ? "" : bibField + "=") +
+                             timeField
+                             + (rankField.empty() ? "" : "/ " + rankField);
+    TextChangeOrder newOrder = buildDefaultChangeOrder(text.c_str());
+    if (NO_VELOCITY_FOR_FIXED_TIMES) newOrder.setVelocity(0);  // override velocity
+    myDisplayer.startChangeOrder(newOrder);
+  }
+  else {
+    // unsure why didn't filter as total time, but do a similar display
+    // combine bib, time, and rank if provided
+    const std::string text = //(bibField.empty() ? "" : bibField + "=") +
+                             timeField
+                             + (rankField.empty() ? "" : "[" + rankField + "]");
     TextChangeOrder newOrder = buildDefaultChangeOrder(text.c_str());
     if (NO_VELOCITY_FOR_FIXED_TIMES) newOrder.setVelocity(0);  // override velocity
     myDisplayer.startChangeOrder(newOrder);
